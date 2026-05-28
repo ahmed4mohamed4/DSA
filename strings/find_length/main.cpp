5c6d44fbbf42070d6553809aaecdc510d56a2c23
Ahmed Mohamed <ahmedmohamed3185@gmail.com>
2026-05-28 21:03:09 +0300

    Complete find len of string function

---
+++ strings/find_length/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int len_str (char s []) {
+    
+    int i;
+
+    for (i = 0; s [i] != '\0'; i++) {}
+
+    return i;
+
+}
+
+int main () {
+    
+    char s [10] = "Ahmed";
+
+    cout << len_str (s) << '\n';
+    
+    return 0;
+}
