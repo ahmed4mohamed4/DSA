30a75a7d94f16bd8d67a16c2303ef7f07d50995c
Ahmed Mohamed <ahmedmohamed3185@gmail.com>
2026-07-06 00:00:04 +0300

    Set up

---
+++ linked_list/single_linked_list/README.md
@@ -0,0 +1,8 @@
+<div align="center">
+    <h1>Single linked list</h1>
+</div>
+<p style="text-align: justify; font-size: 20px;">
+    linear data structure that consists of a sequence of nodes, where each node contains data and a reference (or link) to the next node in the sequence. It is called "single" because each node only points to the next node, unlike a doubly linked list where each node points to both the next and previous nodes.
+</p>
+
+<image src="./images/singly_linked_list.png" alt="Singly linked list"/>
---
+++ linked_list/single_linked_list/display/main.cpp
@@ -0,0 +1,41 @@
+// 🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️
+// 🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️
+// 🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️
+// 
+// 
+// 
+// 
+// ☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔
+// 🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️
+//                                             🕳️
+//                                              😈💀
+// I am just joking ☺️. Don't worry 😈 at all ☺️
+
+#include <iostream>
+
+using namespace std;
+
+/* 
+# Node
+## for single linked list
+- Value : int -> default = 0
+- ptr for next node : Node -> default = nullptr
+*/
+class Node {
+private:
+    int value;
+    Node *ptr;
+public:
+    Node (const int &v = 0) {
+        this->value = v;
+        this->ptr = nullptr;
+    }
+};
+
+
+int main () {
+    
+    
+    
+    return 0;
+}
