b09db158281626d0ce263c75d6afdf676a78205f
Ahmed Mohamed <ahmedmohamed3185@gmail.com>
2026-07-07 01:20:50 +0300

    add isplay function

--- linked_list/single_linked_list/display/main.cpp
+++ linked_list/single_linked_list/display/main.cpp
@@ -1,41 +1,73 @@
-// 🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️🌥️
-// 🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️🌦️
-// 🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️🌧️
-// 
-// 
-// 
-// 
-// ☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔☔
-// 🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️🏃‍♂️‍➡️
-//                                             🕳️
-//                                              😈💀
-// I am just joking ☺️. Don't worry 😈 at all ☺️
 
 #include <iostream>
 
 using namespace std;
 
-/* 
-# Node
-## for single linked list
-- Value : int -> default = 0
-- ptr for next node : Node -> default = nullptr
-*/
 class Node {
-private:
+public:
     int value;
     Node *ptr;
-public:
-    Node (const int &v = 0) {
+    Node (int v = 0) {
         this->value = v;
         this->ptr = nullptr;
     }
 };
 
+class linkedList {
+public:
+    Node *head;
+
+    linkedList () { // constructor
+        this->head = nullptr;
+    }
+
+    /*
+    ## Display function
+    - if it is empty : empty
+    - else : display the whole linked list
+    */
+    void display () {
+        cout << "Linked list: ";
+
+        if (head == nullptr) {
+            cout << "Empty\n";
+            return;
+        }
+
+        Node *temp = head;
+
+        while (temp) {
+            cout << temp->value << ' ';
+            temp = temp->ptr;
+        }
+        
+        cout << '\n';
+    }
+
+    
+
+    ~linkedList () { // destructor
+        Node *temp = head;
+
+        while (temp) {
+            Node *next = temp->ptr;
+            delete temp;
+            temp = next;
+        }
+    }
+};
 
 int main () {
     
-    
+    linkedList linked_list_1;
+    linkedList linked_list_2;
+
+    linked_list_1.head = new Node (1);
+    linked_list_1.head->ptr = new Node (2);
+    linked_list_1.head->ptr->ptr = new Node (3);
+
+    linked_list_1.display ();
+    linked_list_2.display ();
     
     return 0;
 }
